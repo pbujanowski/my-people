31b1b21fadb83ff14e60255ff18137680c7878cc
Patryk Bujanowski <bujanowski.patryk@outlook.com>
2023-02-23 11:17:52 +0100

    Created Identity Web project.

---
+++ MyPeople.Identity.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyPeople.Identity.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
