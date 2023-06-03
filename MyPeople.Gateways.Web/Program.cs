0d41b42bb58ab2ee637a3c71bb822a8579e73f23
Patryk Bujanowski <bujanowski.patryk@outlook.com>
2023-06-03 15:07:25 +0200

    Created gateway API project with Ocelot.

---
+++ MyPeople.Gateways.Web/Program.cs
@@ -0,0 +1,27 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json");
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+app.UseCors();
+
+await app.UseOcelot();
+
+app.Run();
