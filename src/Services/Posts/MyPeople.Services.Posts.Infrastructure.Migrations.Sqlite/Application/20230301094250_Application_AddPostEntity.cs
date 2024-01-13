9d3259f08874ea64866af37dadb320a6dc0c6fb3
Patryk Bujanowski <bujanowski.patryk@outlook.com>
2024-01-13 15:44:09 +0100

    Added missing migrations projects.

---
+++ src/Services/Posts/MyPeople.Services.Posts.Infrastructure.Migrations.Sqlite/Application/20230301094250_Application_AddPostEntity.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyPeople.Services.Posts.Infrastructure.Migrations.Sqlite.Application
+{
+    /// <inheritdoc />
+    public partial class Application_AddPostEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table =>
+                    new
+                    {
+                        Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                        UserId = table.Column<Guid>(type: "TEXT", nullable: false),
+                        Content = table.Column<string>(type: "TEXT", nullable: false),
+                        CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                        UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                    },
+                constraints: table => table.PrimaryKey("PK_Posts", x => x.Id)
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "Posts");
+        }
+    }
+}
