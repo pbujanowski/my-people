104a0c7c91cde74558ba1536ec7c8dba660ae252
Patryk Bujanowski <bujanowski.patryk@outlook.com>
2024-04-02 08:53:08 +0200

    Fixed MappingProfile for Images service and updated NuGet packages.

--- src/Services/Images/MyPeople.Services.Images.Application/Mappings/MappingProfile.cs
+++ src/Services/Images/MyPeople.Services.Images.Application/Mappings/MappingProfile.cs
@@ -10,7 +10,16 @@ public class MappingProfile : Profile
     {
         CreateMap<Image, ImageDto>();
         CreateMap<ImageDto, Image>();
-        CreateMap<CreateImageDto, Image>();
-        CreateMap<DeleteImageDto, Image>();
+        CreateMap<CreateImageDto, Image>()
+            .ForMember(destination => destination.Id, options => options.Ignore())
+            .ForMember(destination => destination.CreatedAt, options => options.Ignore())
+            .ForMember(destination => destination.UpdatedAt, options => options.Ignore());
+        
+        CreateMap<DeleteImageDto, Image>()
+            .ForMember(destination => destination.Name, options => options.Ignore())
+            .ForMember(destination => destination.ContentType, options => options.Ignore())
+            .ForMember(destination => destination.Content, options => options.Ignore())
+            .ForMember(destination => destination.CreatedAt, options => options.Ignore())
+            .ForMember(destination => destination.UpdatedAt, options => options.Ignore());
     }
 }
